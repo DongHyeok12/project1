e427538cffdb8b4b3865cf8a9608b5e3a05bc6c7
김동건 <lllskg@daumsoft.com>
2024-06-28 00:21:57 +0900

    :memo: 김동건 First commit

--- src/component/Listbox.tsx
+++ src/component/Listbox.tsx
@@ -1,63 +1,45 @@
 import { Table, TableColumnsType } from "antd";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { columns } from "../constant";
+
+export interface DataType {
+  key: React.Key;
+  contentNum: number;
+  content: string;
+  writer: string;
+  viewCount: number;
+}
 
 export default function Listbox() {
-  interface DataType {
-    key: React.Key;
-    contentNum: number;
-    content: string;
-    writer: string;
-    viewCount: number;
-  }
-  const columns: TableColumnsType<DataType> = [
-    {
-      title: "번호",
-      dataIndex: "contentNum",
-      width: 15,
-    },
-    {
-      title: "게시글",
-      dataIndex: "content",
-      width: 150,
-    },
-    {
-      title: "작성자",
-      dataIndex: "writer",
-      width: 60,
-    },
-    {
-      title: "조회수",
-      dataIndex: "viewCount",
-      width: 30,
-    },
-  ];
+  const [initialdata, setInitialdate] = useState<DataType[]>([]);
 
-  const initialdata: DataType[] = [];
-  for (let i = 1; i < 100; i++) {
-    initialdata.push({
-      key: i,
-      writer: `익명 ${i}`,
-      contentNum: i,
-      content: `게시글 ${i}`,
-      viewCount: 0,
-    });
-  }
-  const [data, setData] = useState<DataType[]>(initialdata);
+  useEffect(() => {
+    const data: DataType[] = [];
+    for (let i = 1; i < 100; i++) {
+      data.push({
+        key: i,
+        writer: `익명 ${i}`,
+        contentNum: i,
+        content: `게시글 ${i}`,
+        viewCount: 0,
+      });
+    }
+
+    setInitialdate(data);
+  }, []);
 
   const handleRowClick = (index: number) => {
-    const newData = [...data];
+    const newData = [...initialdata];
     newData[index].viewCount += 1;
-    setData(newData);
+    setInitialdate(newData);
   };
-
-  // const [viewCount, setviewCount] = useState(0);
 
   return (
     <>
       <Table
         columns={columns}
-        dataSource={data}
-        onRow={(record, index) => {
+        dataSource={initialdata}
+        onRow={(_, index) => {
           return {
             onClick: () => {
               handleRowClick(index!);
