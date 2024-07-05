d912d5ed00deb861e323a020daec3e9fdf9c2df9
김동건 <lllskg@daumsoft.com>
2024-07-06 01:15:03 +0900

    :sparkles: nav 버튼 동적 클래스 할당

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import "../styles/styles.css"; // 스타일 시트 임포트
+import { useLocation } from "react-router-dom";
 
 export default function NavBar() {
+  const location = useLocation();
+
+  const links = [
+    { path: "/", label: "홈" },
+    { path: "/humor", label: "유머" },
+    { path: "/free", label: "자유" },
+  ];
+
   return (
-    <>
-      <div>
-        <a className="NavBar" href="/">
-          홈
+    <div>
+      {links.map(({ path, label }) => (
+        <a
+          key={path}
+          className={`NavBar${location.pathname === path ? "-Select" : ""}`}
+          href={path}
+        >
+          {label}
         </a>
-        {"     "}
-        <a className="NavBar" href="/humor">
-          유머
-        </a>
-        {"     "}
-        <a className="NavBar" href="/free">
-          자유
-        </a>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
